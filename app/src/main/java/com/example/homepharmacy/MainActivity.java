515f877565cfde98c2e685fc5fe2572e521cfe19
w1ano <83294629+w1ano@users.noreply.github.com>
2021-07-28 09:29:15 +0300

    bottom navigation and navGraph first versions added

--- app/src/main/java/com/example/homepharmacy/MainActivity.java
+++ app/src/main/java/com/example/homepharmacy/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.navigation.NavController;
 import androidx.navigation.NavDirections;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.content.ClipData;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -14,7 +15,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity {
     private static NavController navController;
     private static NavHostFragment navHostFragment;
-    private static BottomNavigationView mainButtomNavigator;
+    private static BottomNavigationView mainBottomNavigator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,22 +25,25 @@ public class MainActivity extends AppCompatActivity {
         navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_main_activity_host_fragment);
         navController = navHostFragment.getNavController();
 
-        mainButtomNavigator = (BottomNavigationView) findViewById(R.id.main_activity_bottom_navigation);
-        mainButtomNavigator.setItemIconTintList(null);
-        mainButtomNavigator.setOnNavigationItemSelectedListener(item -> {
+        mainBottomNavigator = (BottomNavigationView) findViewById(R.id.main_activity_bottom_navigation);
+        mainBottomNavigator.setItemIconTintList(null); //getting rid of background in mainBottomNavigator
+        mainBottomNavigator.setOnNavigationItemSelectedListener(item -> { //setting up a listener for mainBottomNavigator
             switch (item.getItemId()) {
                 case R.id.drugs_bottom_menu_item:
                     if (navController.getCurrentDestination().getId() != R.id.drugs_destination_fragment) {
-                        navController.navigate(R.id.action_calendarFragment_to_drugsFragment);
+                        navController.navigate(R.id.action_calendarFragment_to_drugsFragment); //changing navigation fragment
+                        return true;
                     }
-                    break;
+                    return false;
                 case R.id.calendar_bottom_menu_item:
                     if (navController.getCurrentDestination().getId() != R.id.calendar_destination_fragment) {
                         navController.navigate(R.id.action_drugsFragment_to_calendarFragment);
+                        return true;
                     }
-                    break;
+                    return false;
+                default:
+                    return false;
             }
-            return true;
         });
     }
 }
